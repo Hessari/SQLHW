893cf3e72a040a9c3fa6dd353bf6949ff7f8abdf
M H <hessari@Mohammads-MacBook-Air.local>
2019-04-26 23:46:59 -0700

    Final Commit

--- HessariHW.sql
+++ HessariHW.sql
@@ -85,10 +85,6 @@ ON p.staff_id = s.staff_id
 WHERE payment_date BETWEEN  "20050801" AND "20050901"
 GROUP BY s.staff_id;
 
--- Question6c
-
-
-
 -- Question6d
 SELECT f.title, COUNT(f.title) AS num FROM film AS f
 INNER JOIN inventory AS i 
@@ -130,3 +126,89 @@ INNER JOIN city AS ct
 INNER JOIN country AS co
     ON ct.country_id = co.country_id
 WHERE co.country = "Canada";
+
+-- Question7d
+SELECT title FROM film
+WHERE film_id IN (
+
+SELECT film_id
+    FROM film_category
+    WHERE category_id = (
+
+SELECT category_id
+        FROM category
+        WHERE name = "Family"));
+        
+-- Question7e
+
+SELECT f.title, COUNT(f.title) AS rent_mov
+FROM rental AS r
+INNER JOIN inventory AS i
+    ON r.inventory_id = i.inventory_id
+INNER JOIN film AS f
+    ON i.film_id = f.film_id
+GROUP BY f.title
+ORDER BY rent_mov DESC;
+
+-- Question7f
+SELECT a.address, ct.city, co.country, SUM(p.amount) AS total_revenue
+FROM store AS s
+INNER JOIN address AS a
+    ON s.address_id = a.address_id
+INNER JOIN customer AS c
+    ON s.store_id=c.store_id
+INNER JOIN payment AS p
+    ON p.customer_id = c.customer_id
+INNER JOIN city AS ct
+    ON ct.city_id = a.city_id
+INNER JOIN country AS co
+    ON co.country_id = ct.country_id
+GROUP BY a.address, ct.city, co.country;
+
+-- Question7g
+SELECT s.store_id, a.address, cy.city, co.country FROM store AS s
+INNER JOIN address AS a
+    ON s.address_id = a.address_id
+INNER JOIN city AS cy
+    ON cy.city_id = a.city_id
+INNER JOIN country AS co
+    ON co.country_id = cy.country_id;
+    
+-- Question7h
+
+SELECT c.name, SUM(p.amount) AS gross_revenue
+FROM category AS c
+INNER JOIN film_category AS fc
+    ON c.category_id = fc.category_id
+INNER JOIN inventory AS i
+    ON fc.film_id = i.film_id
+INNER JOIN rental AS r
+    ON i.inventory_id = r.inventory_id
+INNER JOIN payment AS p
+    ON r.rental_id = p.rental_id
+GROUP BY name
+ORDER BY gross_revenue DESC
+LIMIT 5;
+
+-- Question8a
+CREATE VIEW top_five_gen_gross_rev AS
+SELECT c.name, SUM(p.amount) AS gross_rev
+FROM category AS c
+INNER JOIN film_category AS fc
+    ON c.category_id = fc.category_id
+INNER JOIN inventory AS i
+    ON fc.film_id = i.film_id
+INNER JOIN rental AS r
+    ON i.inventory_id = r.inventory_id
+INNER JOIN payment AS p
+    ON r.rental_id = p.rental_id
+GROUP BY name
+LIMIT 5;
+
+-- Question8b
+
+SELECT * FROM top_five_gen_gross_rev;
+
+-- Question8c
+
+DROP VIEW IF EXISTS top_five_gen_gross_rev;
